489783b307e08ea84723074d0771d19280a1749d
Aabro098 <arbinstha71@gmail.com>
2024-05-08 12:55:58 +0545

    i want to connect to the MongoDB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const authRouter = require("./routes/auth");
+
+const DB = "mongodb+srv://arbinstha71:A@bro098@cluster0.wh5tylr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+app.use(authRouter);
+
+monggose
+.connect(DB).then(()=>{
+    console.log("Connection Succcessful !")
+}).catch((e)=>{
+    console.log(e);
+});
 
 const PORT = 3000;
 const app = express();
